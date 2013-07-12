aee9b6812d9294dc53b12f1bf96434b46bece4b2
Christopher Wright <cmwright33@gmail.com>
2013-07-12 15:16:16 -0400

    finished route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsTodo::Application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +40,7 @@ RailsTodo::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
